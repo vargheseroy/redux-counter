08bc98b5c4fdcabaaae35d3a45f5fbc2e253de53
Dell <vargheseroy101@gmail.com>
2020-08-22 00:42:16 -0400

    counter modified

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
-    value: 0,
+    value: 10,
   },
   reducers: {
     increment: state => {
@@ -11,7 +11,7 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
+      state.value += 2;
     },
     decrement: state => {
       state.value -= 1;
@@ -19,10 +19,13 @@ export const counterSlice = createSlice({
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount,decrementByAmount } = counterSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
@@ -34,9 +37,16 @@ export const incrementAsync = amount => dispatch => {
   }, 1000);
 };
 
+export const decrementAsync = amount => dispatch => {
+  setTimeout(() => {
+    dispatch(decrementByAmount(amount));
+  }, 1000);
+};
+
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectCount = state => state.counter.value;
+
 
 export default counterSlice.reducer;
